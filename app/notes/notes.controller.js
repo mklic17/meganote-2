2150ce2f9e26ab181ca9d6af550a38ff5b435905
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-07-07 13:39:54 -0400

    replace IFEE with blocks and block scope variables.

---
+++ app/notes/notes.controller.js
@@ -0,0 +1,14 @@
+{
+
+  angular
+    .module('meganote.notes')
+    .controller('NotesController', NotesController);
+
+
+  NotesController.$inject = ['NotesService'];
+
+  function NotesController(NotesService) {
+    const vm = this;
+    vm.notes = NotesService.notes;
+  }
+}
